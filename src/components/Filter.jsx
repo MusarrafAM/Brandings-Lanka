56b2381994f9c2eee66ce2b3c617ee761ca043b1
Musarraf <muhammedmusarraf@gmail.com>
2024-10-29 16:36:12 +0530

    Added shop page content s and filtercomponent and searchbar

---
+++ src/components/Filter.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import Slider from "@mui/material/Slider";
+
+const Filter = () => {
+  const [searchText, setSearchText] = useState("");
+  const [priceRange, setPriceRange] = useState([0, 10000]);
+
+  const onSearch = () => {
+    setSearchText("");
+  };
+
+  const handlePriceChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
+  const getPriceRange = () => {
+    console.log(priceRange);
+  };
+
+  return (
+    <div className="px-6">
+      {/* Searchbar */}
+      <div className="flex items-center pt-2">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="border-2 border-black px-2 py-1"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+        />
+        <button onClick={onSearch} className="bg-red-500 p-2">
+          <IoSearch size={20} />
+        </button>
+      </div>
+
+      {/* Product Categories */}
+      <div className="mt-4">
+        <h4 className=" text-xl text-gray-600 my-6 uppercase font-semibold">
+          Product categories
+        </h4>
+
+        {/* Each Categories  need to dynamically map later*/}
+        <div className="flex items-center justify-between border-b border-gray-300 py-3 group cursor-pointer">
+          <p className="text-gray-600 group-hover:text-black duration-200">
+            Mens collection
+          </p>
+          <p className="text-xs text-gray-400">(102)</p>
+        </div>
+
+        <div className="flex items-center justify-between border-b border-gray-300 py-3 group cursor-pointer">
+          <p className="text-gray-600 group-hover:text-black duration-200">
+            Junior collection
+          </p>
+          <p className="text-xs text-gray-400">(56)</p>
+        </div>
+
+        <div className="flex items-center justify-between border-b border-gray-300 py-3 group cursor-pointer">
+          <p className="text-gray-600 group-hover:text-black duration-200">
+            Women’s collection.
+          </p>
+          <p className="text-xs text-gray-400">(74)</p>
+        </div>
+      </div>
+
+      {/* Filter By Price */}
+      <div className="mt-10">
+        <h4 className="text-xl text-gray-600 uppercase font-semibold">
+          Filter by price
+        </h4>
+        <div className="mt-4">
+          <Slider
+            value={priceRange}
+            onChange={handlePriceChange}
+            valueLabelDisplay="auto"
+            min={0}
+            max={10000}
+            step={100}
+          />
+          <div className="flex justify-between mt-2 text-gray-600">
+            <span>Rs. {priceRange[0]}</span>
+            <span>Rs. {priceRange[1]}</span>
+          </div>
+        </div>
+        <button
+          onClick={getPriceRange}
+          className="bg-blue-400 px-6 py-1 rounded-full mx-auto block"
+        >
+          {/* here block make the button take sthe full width so our mx-auto works as exected */}
+          Filter
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Filter;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,6 +1,27 @@
+import products from "../data/products.json";
+import EachGlassCard from "../components/EachGlassCard";
+import Filter from "../components/Filter";
+
 const Shop = () => {
   return (
-    <div className="h-screen">Shop</div>
-  )
-}
-export default Shop
+    <div className="px-2 lg:px-[10%] py-10">
+      <div className="grid grid-cols-4">
+        <div className="hidden md:block">
+          <Filter />
+        </div>
+        <div className="col-span-4 md:col-span-3">
+          <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+            {products.map((product) => (
+              <EachGlassCard
+                key={product.productName}
+                productName={product.productName}
+                price={product.price}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Shop;
